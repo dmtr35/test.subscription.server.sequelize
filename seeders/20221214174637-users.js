891cc9c555dfded872cf0d6db2cf5b4f68c55d20
dmtr35 <nuke-id@ukr.net>
2022-12-17 16:49:33 +0200

    first

---
+++ seeders/20221214174637-users.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
